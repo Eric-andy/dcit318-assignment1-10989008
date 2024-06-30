58ecfb9d9adb3558ac6891d35e4fbdc770a92afe
Andy Danso <ericandy500@outlook.com>
2024-06-30 04:23:22 +0400

    completed GradeCal app

---
+++ GradeCal/GradeCal/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCal
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			{
+				Console.Write("Enter a numerical grade (0-100): ");
+				int grade = int.Parse(Console.ReadLine());
+
+				if (grade >= 90)
+				{
+					Console.WriteLine("Letter grade: A");
+				}
+				else if (grade >= 80)
+				{
+					Console.WriteLine("Letter grade: B");
+				}
+				else if (grade >= 70)
+				{
+					Console.WriteLine("Letter grade: C");
+				}
+				else if (grade >= 60)
+				{
+					Console.WriteLine("Letter grade: D");
+				}
+				else
+				{
+					Console.WriteLine("Letter grade: F");
+				}
+			}
+		}
+	}
+}
