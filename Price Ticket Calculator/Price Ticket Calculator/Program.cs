be9688069cb603828dd4e4982edc53ee26abf226
Andy Danso <ericandy500@outlook.com>
2024-06-30 04:45:33 +0400

    completed Price ticket calculator

---
+++ Price Ticket Calculator/Price Ticket Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price_Ticket_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            int ticketPrice = (age <= 12 || age >= 65) ? 7 : 10;
+            Console.WriteLine("Ticket price: GHC" + ticketPrice);
+        }
+    }
+}
+    }
+}
